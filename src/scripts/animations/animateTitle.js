d0eb7d78d326d9b74c14e1b04d381b4fc198947d
LuanVictorGit <luanvictorchagas2015@gmail.com>
2025-03-17 13:04:30 -0300

    first commit

---
+++ src/scripts/animations/animateTitle.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+    updateTitles(); // init the animations
+});
+
+let typingSpeed = 50;
+let eraseSpeed = 30;
+let pauseBeforeErase = 250;
+let pauseAfterTyping = 5000;
+
+function updateTitles() {
+    let titles = document.querySelectorAll("*");
+    titles.forEach((title, index) => {
+        if (!title.classList.contains("animation-title")) return;
+        let textTitle = title.textContent.trim();
+        title.textContent = "";
+        typeAndErase(title, textTitle, index * 20);
+    });
+}
+
+function typeAndErase(title, textTitle, initialDelay) {
+    setTimeout(() => {
+        typeTitle(title, textTitle, 0);
+    }, initialDelay);
+}
+
+function typeTitle(title, textTitle, currentLength) {
+    if (currentLength < textTitle.length) {
+        title.textContent += textTitle[currentLength];
+        setTimeout(() => {
+            typeTitle(title, textTitle, currentLength + 1);
+        }, typingSpeed);
+    } else {
+        setTimeout(() => {
+            eraseTitle(title, textTitle);
+        }, pauseAfterTyping);
+    }
+}
+
+function eraseTitle(title, textTitle) {
+    if (title.textContent.length > 0) {
+        title.textContent = title.textContent.slice(0, -1);
+        setTimeout(() => {
+            eraseTitle(title, textTitle);
+        }, eraseSpeed);
+    } else {
+        setTimeout(() => {
+            typeTitle(title, textTitle, 0);
+        }, pauseBeforeErase);
+    }
+}
